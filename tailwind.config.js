e1800c107dbdc0e3f1fc4a51ae8604a33df371ba
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-07-11 17:00:28 -0400

    Began on Atlas

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#19122E",
+        secondary: "#685AAB",
+        offwhite: "#F4F4F4",
+      },
+      // fontFamily: {
+      //   poppins: ["Poppins", "sans-serif"],
+      // },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
