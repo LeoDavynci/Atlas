42beb4fc138a3b00ea5ed1653687947946876272
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-07-13 14:43:54 -0400

    Started on UI

--- Atlas/src/App.tsx
+++ Atlas/src/App.tsx
@@ -1,13 +1,15 @@
 
 
-import './App.css'
-
 function App() {
 
   return (
     <>
-      <div>
-        <p>Hello</p>
+      <div className='appGradient w-full overflow-hidden'>
+        <div className="bg-gray-600 w-1/2">
+          <p>
+            hello
+          </p>
+        </div>
       </div>
     </>
   )
