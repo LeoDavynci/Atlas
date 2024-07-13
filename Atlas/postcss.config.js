977516901ad88e64cfe0409a43012123b78ceec4
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-07-12 21:33:06 -0400

    Started on css

--- Atlas/postcss.config.js
+++ Atlas/postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+import autoprefixer from 'autoprefixer';
+import tailwindcss from 'tailwindcss';
+
+export default {
+  plugins: [
+    tailwindcss,
+    autoprefixer,
+  ]
+};
