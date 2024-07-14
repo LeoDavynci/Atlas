fb78973ee3dc46867866778e12edfb2e3b681574
LeoDavynci <106774605+LeoDavynci@users.noreply.github.com>
2024-07-14 12:03:40 -0400

    Implemented auth backend

--- Atlas/src/functions/auth.js
+++ Atlas/src/functions/auth.js
@@ -3,4 +3,34 @@ import {
     signInWithEmailAndPassword,
   } from "firebase/auth";
   import { doc, setDoc } from "firebase/firestore";
-  import { auth, db } from "../firebase/fire";
+import { auth, db } from "../firebase/firebase";
+
+export const signIn = async (email, password) => {
+    try {
+      const res = await signInWithEmailAndPassword(auth, email, password);
+      return res.user;
+    } catch (error) {
+      return error;
+    }
+};
+  
+export const logout = async () => {
+    try {
+      await auth.signOut();
+    } catch (error) {
+      return error;
+    }
+};
+  
+export const signUp = async (username, name, email, password, userData) => {
+    try {
+      const user = await createUserWithEmailAndPassword(auth, username, name, email, password);
+      await setDoc(doc(db, "users", user.user.uid), {
+        ...userData,
+        uid: user.user.uid,
+      });
+      return user.user;
+    } catch (error) {
+      return error;
+    }
+  };
